cf915e80872024f09959d094aca574697b4b4c7d
Paleo <573793638@qq.com>
2019-07-13 15:02:16 +0800

    add

--- api/zhihu/opt/AnsOpt.py
+++ api/zhihu/opt/AnsOpt.py
@@ -1,7 +1,14 @@
-from api.zhihu.utils.URL import URL
+import re
 
 
 # 提取知乎回答内容的图片url
 def get_img_urls(content):
-    urls = URL.find_url(content)
+    urls = find_url(content)
     return urls
+
+
+
+def find_url(string):
+    # findall() 查找匹配正则表达式的字符串 data-original 原始图片
+    url = re.findall('data-original=\\"https://(.*?)\\"', string)
+    return url
--- api/zhihu/utils/URL.py
+++ api/zhihu/utils/URL.py
@@ -1,15 +1,6 @@
-import re
-
-
 class URL(object):
     host = "https://www.zhihu.com"
     zhuanlan_host = "https://zhuanlan.zhihu.com"
-
-    @staticmethod
-    def find_url(string):
-        # findall() 查找匹配正则表达式的字符串
-        url = re.findall('src=\\"https://(.*?)\\"', string)
-        return url
 
     @classmethod
     def index(cls):
